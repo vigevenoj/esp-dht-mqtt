89fad202cf00a3f94385977d7effff59b8be89dc
EAD Fritz <ead.fritz@gmail.com>
2015-03-16 16:41:32 +0100

    sensor types are now defined in user_config.h

--- examples/console/user/user_main.c
+++ examples/console/user/user_main.c
@@ -19,9 +19,10 @@
 #include <os_type.h>
 #include "stdout/stdout.h"
 #include "dhtxx/dhtxx.h"
+#include "user_config.h"
 
-#define DHT_SAMPLE_PERIOD 500 /* 0.5 seconds between each sensor x7 sensors = 3.5 seconds period on each sensor  */
-#define DHT_NUMBER_OF_SENSORS 7
+#define DHT_SAMPLE_PERIOD 500 /* 0.5 seconds between each sensor x8 sensors = 4 seconds period on each sensor  */
+#define DHT_NUMBER_OF_SENSORS 8
 
 static os_timer_t dht22_timer;
 DHT_Sensor sensors[DHT_NUMBER_OF_SENSORS];
@@ -46,13 +47,14 @@ dht22_loop(void) {
 void ICACHE_FLASH_ATTR
 dht22_setup(void) {
 
-  dht_init(sensors+0, DHT22, 0);  // GPIO0
-  dht_init(sensors+1, DHT22, 2);  // GPIO2
-  dht_init(sensors+2, DHT22, 4);  // GPIO4
-  dht_init(sensors+3, DHT22, 5);  // GPIO5
-  dht_init(sensors+4, DHT22, 12); // GPIO12
-  dht_init(sensors+5, DHT22, 13); // GPIO13
-  dht_init(sensors+6, DHT22, 14); // GPIO14
+  dht_init(sensors+0, GPIO0_DHT_TYPE, 0);  // GPIO0
+  dht_init(sensors+1, GPIO2_DHT_TYPE, 2);  // GPIO2
+  dht_init(sensors+2, GPIO4_DHT_TYPE, 4);  // GPIO4
+  dht_init(sensors+3, GPIO5_DHT_TYPE, 5);  // GPIO5
+  dht_init(sensors+4, GPIO12_DHT_TYPE, 12); // GPIO12
+  dht_init(sensors+5, GPIO13_DHT_TYPE, 13); // GPIO13
+  dht_init(sensors+6, GPIO14_DHT_TYPE, 14); // GPIO14
+  dht_init(sensors+7, GPIO16_DHT_TYPE, 16); // GPIO16
 
   os_timer_disarm(&dht22_timer);
   os_timer_setfn(&dht22_timer, (os_timer_func_t *)dht22_loop, NULL);
